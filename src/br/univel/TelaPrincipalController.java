a2d5515bce845b54b08e42bc6f2f760e8b65ac1a
Ricardo Cotienschi <ricardocotienschi@live.com>
2016-03-22 20:38:45 -0300

    Primeiro Exercicio FX

---
+++ src/br/univel/TelaPrincipalController.java
@@ -0,0 +1,21 @@
+package br.univel;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class TelaPrincipalController {
+
+    @FXML
+    private TextField campo;
+
+    @FXML
+    private Button botao;
+
+    @FXML
+    void executar(ActionEvent event) {
+    	System.out.println(campo.getText());
+    }
+
+}
